1ce0426ad4e9c29bb4c92a61c8cfd34f03ac741d
Umar Hamza <umar@umarhamza.com>
2015-09-28 09:47:13 +0100

    added new deps, browsersync & bourbon

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,78 @@
-// load in gulp tasks
-var gulp 		= require('gulp');
-var sass 		= require('gulp-sass');
+var gulp 			= require('gulp');
+var sass 			= require('gulp-sass');
+var sourcemaps 		= require('gulp-sourcemaps');
+var uglify 			= require('gulp-uglify');
+var browserSync 	= require('browser-sync').create();
 
-// Gulp Sass Task
+// Gulp BrowserSync Task
+// Static Server + watching scss/html files
+gulp.task('serve', function() {
+
+	// initialise browserSync
+	browserSync.init({
+		
+		// add a server dir
+		server: {
+			baseDir: "./"
+		},// server
+
+		// add a proxy address to your server
+		//proxy: "mywebsite.dev",
+
+		// select a default browser
+		browser: ["chrome"]
+
+	});// browser sync init
+
+	// watch files and when it changes, run tasks
+  	gulp.watch('./_/components/sass/**/*.{scss,sass}', ['sass']);
+  	gulp.watch('./_/components/js/*.js', ['uglify']);
+  	gulp.watch('*.html').on('change', browserSync.reload);
+
+
+});// gulp browsersync task
+
+// Gulp js Task
+gulp.task('uglify', function() {
+
+	// find the js files and return
+	return gulp.src('./_/components/js/*.js')
+
+	// compress the js files
+	.pipe(uglify())
+	
+	// output js into js dir
+	.pipe(gulp.dest('./_/js'))
+	
+	// add to browsersync stream & live reload using BrowserSync
+	.pipe( browserSync.stream() );
+
+}); // uglify
+
+// Gulp Sass Task 
 gulp.task('sass', function() {
+
+	// find the sass files and extentions. return make browserSync wait for sass to be processed
+	return gulp.src('./_/components/sass/**/*.{scss,sass}')
+
+	// initialise sourcemaps
+	.pipe(sourcemaps.init())
 	
-	// Find the criminal sass files	(sass police)
-	gulp.src('_/components/sass/*.{sass,scss}')
+	// compile sass into css. Also add errors to console
+	.pipe(sass({
+		errLogToConsole: true
+	}))
 
-	// run the sass tasks 
-	.pipe(sass())
+	// compile sass the source map 
+	.pipe(sourcemaps.write())
 
-	// output the sass into the css file
-	.pipe(gulp.dest('_/css'))
+	// output css into the css dir
+	.pipe(gulp.dest('./_/css'))
 
-})//sass task
+	// add to browsersync stream & live reload using BrowserSync
+	.pipe( browserSync.stream() );
 
-gulp.task('watch', function() {
-	
-	// neighbourhood watch
-	gulp.watch('_/components/sass/*.{sass,scss}', ['sass']);
-}); // sass task
+});//gulp sass
 
-gulp.task('default', ['sass', 'watch']);
+// Create Gulp Default Task
+gulp.task('default', ['serve']);
