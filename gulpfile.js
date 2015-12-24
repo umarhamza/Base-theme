552b2cc2952bb235db5298aa2c80e99faccc2c5b
Umar Hamza <umar@umarhamza.com>
2015-12-24 16:20:37 +0000

    working on responsive

--- gulpfile.js
+++ gulpfile.js
@@ -62,7 +62,8 @@ gulp.task('sass', function() {
 	
 	// compile sass into css. Also add errors to console
 	.pipe(sass({
-		errLogToConsole: true
+		errLogToConsole: true,
+		outputStyle: 'compact' // :nested :compact :expanded :compressed
 	}))
 
 	.on('error', onError)
