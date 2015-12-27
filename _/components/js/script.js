ef733912b37a69858bae47a368444ec3a086eba4
Umar Hamza <umar@umarhamza.com>
2015-12-27 09:41:01 +0000

    working on carousel

--- _/components/js/script.js
+++ _/components/js/script.js
@@ -51,7 +51,17 @@ mobileMenu.addEventListener('click', function() {
 
 		var direction = e.target.getAttribute('data-dir');
 		// update current value
-		(direction === 'next') ? ++current : --current;  //terinery operator for if (direction === 'next') { current += 1; } else {current -= 1};
+		//(direction === 'next') ? ++current : --current;  //terinery operator for
+
+		if (direction === 'next') {
+			current += 1;
+		}
+		else {
+			current -= 1;
+			console.log('before: ' + loc);
+			loc -= widthMargin;
+			console.log('after: ' + loc);
+		};
 
 		// if first image
 		if (current === 0) { // are we going beyond the first image
@@ -63,7 +73,11 @@ mobileMenu.addEventListener('click', function() {
 			loc = 0;
 		}
 
+		output(e);
+
+
 		transition(carousel, direction, widthMargin);
+
 
 // my code : moving things forward
 // carousel.style.marginLeft = '-' + counter + 'px';
@@ -72,44 +86,91 @@ mobileMenu.addEventListener('click', function() {
 		}; // if has class arrow
 	}, false);
 
-//transition function
-function transition( container, direction, width ) {
+	//transition function
+	function transition( container, direction, width ) {
 
-	var unit; // - or +
+		var unit; // - or +
 
-	if (direction && loc !== 0) {
-		unit = (direction === 'next') ? '-' : '+';
-	};
-
-	container.style.marginLeft = '-' + loc + 'px';
-
-	loc += width;
-
-}
-
-})();
-
-// scroll func
-(function() {
-	//"use strict"
-
-	var header = document.querySelector('#header'),
-		shrinkOn = 600;
-
-		// var mq = window.matchMedia( "(min-width: 997px)" );
-		// if (mq.matches) { } // JS Media query
-
-	window.addEventListener('scroll', function scrollFunc() {
-
-		var distanceY = window.pageYOffset || document.documentElement.scrollTop;
-
-		if (distanceY > shrinkOn) {
-			header.className = 'shrink-header';
-			//window.removeEventListener(scroll, scrollFunc, false);
-		} else {
-			header.removeAttribute('class', 'shrink-header');
-
+		if (direction && loc !== 0) {
+			unit = (direction === 'next') ? '-' : '+';
 		};
 
-	}, false); // scroll evt listener
-})();
+		container.style.marginLeft = '-' + loc + 'px';
+
+		loc += width;
+
+	} // transition func
+
+		// output panel show
+		var outputPanel = document.createElement('div'),
+				d_direction = document.createElement('div'),
+				d_loc = document.createElement('div'),
+				d_totalImgWidth = document.createElement('div'),
+				d_current = document.createElement('div'),
+				carouselRow = document.getElementById('row5');
+
+		d_direction.id = 'd_direction';
+		d_loc.id = 'd_loc';
+		d_totalImgWidth.id = 'd_totalImgWidth';
+		d_current.id = 'd_current';
+
+		// styling panel
+		outputPanel.innerHTML = 'This is the output panel';
+		outputPanel.style.border = '1px solid tomato';
+		outputPanel.style.background = 'white';
+		outputPanel.style.position = 'absolute';
+		outputPanel.style.top = '0px';
+		outputPanel.style.height = '150px';
+		outputPanel.style.width = '300px';
+
+		// adding panel to row
+		carouselRow.appendChild(outputPanel);
+
+		outputPanel.appendChild(d_direction);
+		outputPanel.appendChild(d_loc);
+		outputPanel.appendChild(d_totalImgWidth);
+		outputPanel.appendChild(d_current);
+
+		function output(evt) {
+
+				// adding stuff to panel
+				// outputPanel.appendChild(loc);
+				// outputPanel.appendChild(totalImgWidth);
+				d_direction.innerHTML = 'Direction: ' + evt.target.getAttribute('data-dir');
+				d_loc.innerHTML = 'Location: ' + loc;
+				d_totalImgWidth.innerHTML = 'Moved along: ' + totalImgWidth;
+				d_current.innerHTML = 'Current count: ' + current;
+
+			//	console.log(direction);
+				console.log("console");
+
+		} // output panel
+
+
+})(); // ife carousel
+
+// scroll func
+// (function() {
+// 	"use strict"
+//
+// 	var header = document.querySelector('#header'),
+// 		shrinkOn = 600;
+//
+// 	// JS Media Query
+// 	// mq = window.matchMedia( "(min-width: 997px)" );
+// 	// if (mq.matches) {} // JS Media Query
+//
+// 	window.addEventListener('scroll', function scrollFunc() {
+//
+// 			var distanceY = window.pageYOffset || document.documentElement.scrollTop;
+//
+// 			if (distanceY > shrinkOn) {
+// 				header.className = 'shrink-header';
+// 				//window.removeEventListener(scroll, scrollFunc, false);
+// 			} else {
+// 				header.removeAttribute('class', 'shrink-header');
+//
+// 			};
+//
+// 	}, false); // scroll evt listener
+// })(); // scroll ife
